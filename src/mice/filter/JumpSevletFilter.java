2e98068a42f7b6e7d884643775b3c5c7587c4ee0
DogA <1661073664@qq.com>
2018-11-13 14:45:16 +0800

    JumpSevlet

---
+++ src/mice/filter/JumpSevletFilter.java
@@ -0,0 +1,35 @@
+package mice.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JumpSevletFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String contextPath = request.getServletContext().getContextPath();
+        request.getServletContext().setAttribute("contextPath", contextPath);
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+
+    }
+
+}
